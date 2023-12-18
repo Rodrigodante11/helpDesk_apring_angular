d2c32376d448533540a22354d4ff6758a9fb2003
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-12-18 19:34:32 -0300

    perfil de desenvolvimento e banco Postgres

--- src/main/java/com/rodrigo/helpdesk/config/TestConfig.java
+++ src/main/java/com/rodrigo/helpdesk/config/TestConfig.java
@@ -2,19 +2,26 @@ package com.rodrigo.helpdesk.config;
 
 import com.rodrigo.helpdesk.services.DBServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig {
 
     @Autowired
     private DBServices dbServices;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
     @Bean  // quando test config for instanciado o metodo com Bean vai rodar automaticamente
-    public void instanciaDB(){
-        this.dbServices.instanciaDB();
+    public boolean instanciaDB(){
+        if(value.equals("create")){
+            this.dbServices.instanciaDB();
+        }
+
+        return false;
     }
 }
