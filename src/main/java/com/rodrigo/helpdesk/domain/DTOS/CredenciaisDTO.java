1292d2db749e046ffdb6e68d220121bcaa543347
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-02-13 14:40:07 -0300

    adicionando criacao de token com expiracao

---
+++ src/main/java/com/rodrigo/helpdesk/domain/DTOS/CredenciaisDTO.java
@@ -0,0 +1,12 @@
+package com.rodrigo.helpdesk.domain.DTOS;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CredenciaisDTO {
+
+    private String email;
+    private String senha;
+}
