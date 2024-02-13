21096c232f8c88477ecfa8c414528828365215ee
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-02-13 13:48:01 -0300

    adicionando Spring Secutiry

---
+++ src/main/java/com/rodrigo/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.rodrigo.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final  String[] PUBLIC_MATCHERS = {"/h2-console/**"};
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){  // permintindo o h2 console caso for profile de "test"
+            http.headers().frameOptions().disable();
+        }
+
+
+        http.cors().and().csrf().disable(); // por causa do @beans vai pegar o metodo abaixo automaticamente(corsConfigurationSource())
+
+        http.authorizeRequests()
+                        .antMatchers(PUBLIC_MATCHERS).permitAll() // esse nao precisa de autenticacao
+                        .anyRequest().authenticated(); // mas todo o resto precisa
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST", "GET" , "PUT", "DELETE", "OPTIONS"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**" , corsConfiguration);
+        return source;
+    }
+}
